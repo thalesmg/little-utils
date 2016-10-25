0d9b64b9e7a743082568dcc6d947b793716f29ec
Thales Macedo Garitezi <thalesmg@gmail.com>
2016-10-24 22:44:09 -0200

    Adding a few comments

--- split.hs
+++ split.hs
@@ -1,16 +1,24 @@
+module Split where
+
 import Control.Applicative
 import Data.List
 import System.Environment
 import System.IO
 
-splitar :: Int -> [a] -> [[a]]
-splitar n lst = [[e | (e, i) <- zip lst [0..], i `mod` n == j] | j <- [0..n-1]]
+{-
+Split a list of objects into n lists.
+-}
+split :: Int -- ^Number of resulting lists
+      -> [a] -- ^Original list of elements
+      -> [[a]]
+split n lst = [[e | (e, i) <- zip lst [0..], i `mod` n == j] | j <- [0..n-1]]
 
 
 main :: IO ()
 main = do
-  [n, arq] <- getArgs
-  conteudo <- lines <$> readFile arq
-  let lstlst = splitar (read n) conteudo
-      nomes = zipWith (\s num -> s ++ (show num)) (repeat arq) [1..]
-  mapM_ (\(arq', ls) -> writeFile arq' (unlines ls)) (zip nomes lstlst)
+  [n, file] <- getArgs
+  when n <= 0 $ error "The number of resulting files must be greater than 1."
+  content <- lines <$> readFile file
+  let lstlst = split (read n) content
+      names = zipWith (\s num -> s ++ (show num)) (repeat file) [1..]
+  mapM_ (\(file', ls) -> writeFile file' (unlines ls)) (zip names lstlst)
